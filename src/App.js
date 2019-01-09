c7a69f2e05bb5e75d4b68d2c6bd7e5e1c5eab298
aaron <aaronakamm@gmail.com>
2019-01-09 13:02:17 -0800

    Set up event handler to receive data after entering zip code

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import Titles from './components/Titles.js';
 import Form from './components/Form.js';
 import Weather from './components/Weather.js'
 
+const apiKey = "fcd1d3be3229265b6c5bdfd66d9a8078";
+
 class App extends React.Component {
+ 
+  constructor(props){
+    super(props);
+    this.getWeather = this.getWeather.bind(this);
+  }
+
+  async getWeather(e) {
+    e.preventDefault();
+    const zip = e.target.elements.zip.value.trim();
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${apiKey}&units=imperial`);
+    const data = await res.json();
+    console.log(data);
+  }
+  
   render(){
     return (
       <div>
         <Titles />
-        <Form />
+        <Form getWeather = {this.getWeather} />
         <Weather />
       </div>
     )
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const Form = () => {
+const Form = (props) => {
     return(
-        <div>Form</div>
+        <form onSubmit = {props.getWeather}>
+            <input type = "number" name = "zip" placeholder = "Enter Zip Code"/>
+            <button>Get Weather</button>
+        </form>
     )
 }
 export default Form;
